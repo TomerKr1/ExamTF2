e4cdd588784677903f3a56d4fc60a48f9052b35f
TomerKr1 <tomer.kraft1@gmail.com>
2025-02-07 22:08:15 +0200

    Commit

--- network.tf
+++ network.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"  
-    gateway_id = aws_internet_gateway.custom_gateWay.id
+    gateway_id = aws_internet_gateway.custom_gateWay.id  // the VPC will recognized we trying to get to an ip that outsite our VPC ZONE!
   }
 
   tags = {
